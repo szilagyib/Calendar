12c77241ee044bda5402f73b0a45e1b532b040b9
szilagyib <47719445+szilagyib@users.noreply.github.com>
2022-06-28 00:26:23 +0200

    Update EventList.java

--- src/cal/EventList.java
+++ src/cal/EventList.java
@@ -3,37 +3,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
 
-public class EventList
-{
+public class EventList {
 	protected List<Event> events;
 	
-	public EventList()
-	{
+	public EventList() {
 		events = new ArrayList<>();
 	}
 	
-	public void addEvent(Event e)
-	{
+	public void addEvent(Event e) {
 		deleteEvent(e);
 		events.add(e);
 	}
 	
-	//for-each ciklussal ConcurrentModificationException
-	public void deleteEvent(Event e)
-	{
+	public void deleteEvent(Event e) {
 		Iterator<Event> iter = events.iterator();
-		while(iter.hasNext())
-		{
+		while(iter.hasNext()) {
 		    Event i = iter.next();
 		    if( i.equals(e))
 		        iter.remove();
 		}
 	}
 	
-	public String shareEvent(Event e)
-	{
-		for (Event i : events)
-		{
+	public String shareEvent(Event e) {
+		for (Event i : events) {
 			if (i.equals(e))
 				return i.getName();
 		}
